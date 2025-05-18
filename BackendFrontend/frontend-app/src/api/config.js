561d76b5a06eda80606413018aa800b51c902431
ENES BURAK ATAY <devenesburakatay@gmail.com>
2025-05-18 21:56:43 +0300

    Links are dynamized for accessing from different devices.

--- BackendFrontend/frontend-app/src/api/config.js
+++ BackendFrontend/frontend-app/src/api/config.js
@@ -1,6 +1,6 @@
 
-const BASE_URL = '192.168.1.8'
-// const BASE_URL = 'localhost'
+// const BASE_URL = '192.168.1.8'
+const BASE_URL = 'localhost'
 const API_BASE_URL = `http://${BASE_URL}:5000/api`;
 const GOOGLE_API_KEY = "YOURGOOGLEBOOKAPI"; // Replace with your actual API key if needed
 
