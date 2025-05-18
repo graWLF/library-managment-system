e4b78b2daf4c4acd359bbbb5ba65f2652009f9a8
ENES BURAK ATAY <devenesburakatay@gmail.com>
2025-05-18 22:01:20 +0300

    Links are dynamized for accessing from different devices. Barcode Upload is in process

--- Mobile/api/apiConfig.ts
+++ Mobile/api/apiConfig.ts
@@ -1,2 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = '192.168.1.8'
+const BASE_URL = 'localhost'
+const API_BASE_URL = `http://${BASE_URL}:5000/api`;
 export default API_BASE_URL;
