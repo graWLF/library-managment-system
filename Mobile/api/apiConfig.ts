f36d68d5968be838340458d0a6f7684f3c1e9a4a
Deniz Eren Arıcı <denizerenarici@gmail.com>
2025-05-21 19:13:41 +0300

    Routinp protection and tabs navigation fixed.

--- Mobile/api/apiConfig.ts
+++ Mobile/api/apiConfig.ts
@@ -1,4 +1,4 @@
 // const BASE_URL = '192.168.1.8'
-const BASE_URL = '192.168.15.24'
+const BASE_URL = '192.168.117.24'
 const API_BASE_URL = `http://${BASE_URL}:5000/api`;
 export default API_BASE_URL;
